0e4325553cfa3a840e767271df2b34f06558d364
IsmailG <ismailguerraoui04@gmail.com>
2026-01-20 15:25:54 +0100

    #2 Refactor du routeur -> passage par un middleWare

--- services/gameService/router.js
+++ services/gameService/router.js
@@ -1,34 +1,15 @@
 const Board = require('./entities/board');
 const BoardManager = require('./manager/boardManager')
 const readJsonBody= require('../helpers/parser.js');
-
-const boardManager = new BoardManager();
+const handler = require('./handler.js');
 
 const routes = {
-    '/api/init-board': (req, res) => {boardManager.initBoard(req, res)},
-    '/api/place' : async (req,res) => {
-        try {
-            const { x, y, piece } = await readJsonBody(req);
-            const result = boardManager.placePiece(x, y, piece);
-
-            res.writeHead(result.ok ? 200 : 400, { "Content-Type": "application/json" });
-            res.end(JSON.stringify(result));
-        } catch (e) {
-            res.writeHead(400, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ ok: false, error: "INVALID_JSON" }));
-        }
+    '/api/init-board': (req, res) => {
+        //méthode du middleWare
+        handler.initBoard(req, res);
     },
-    '/api/move' : async (req,res) => {
-        try{
-            const { x,y,owner,fromX,fromY} = await readJsonBody(req);
-            const result = boardManager.movePiece(x,y,fromX,fromY);
-            res.writeHead(result.ok ? 200 : 400, { "Content-Type": "application/json" });
-            res.end(JSON.stringify(result));
-        } catch (e) {
-            res.writeHead(400, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ ok: false, error: "INVALID_JSON" }));
-        }
-    }
+    '/api/place' : (req,res) => handler.placePiece(req,res),
+    '/api/move' : (req,res) => handler.movePiece(req,res)
 };
 
 async function manage(request,response){
