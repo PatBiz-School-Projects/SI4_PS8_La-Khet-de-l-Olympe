c92b63eca7cc569939313659ac6f6a6b02b9f2db
PatBiz <patrick.bizot@etu.univ-cotedazur.fr>
2026-02-15 21:43:04 +0100

    chore(front): enumerated `GameAction`

---
+++ services/files/front/pages/game-page/GamePageStateMachine/GameAction.js
@@ -0,0 +1,13 @@
+/**
+ * @enum {typeof GameActionType[keyof typeof GameActionType]}
+ */
+export const GameActionType = /** @type {const} */ ({
+    MOVE_PIECE: "MOVE_PIECE",
+    PLACE_PIECE: "PLACE_PIECE",
+    ROTATE_PIECE: "ROTATE_PIECE",
+    SWITCH_PIECES: "SWITCH_PIECES",
+});
+
+export const GameActionPayload = undefined;
+
+export const GameAction = undefined;
