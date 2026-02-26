fc9c2ee6cd1436ee165f80bcbd089359ba002d3d
PatBiz <patrick.bizot@etu.univ-cotedazur.fr>
2026-02-26 20:07:17 +0100

    chore(front): adapted home page

--- services/files/front/pages/home-page/home-page.js
+++ services/files/front/pages/home-page/home-page.js
@@ -3,17 +3,39 @@ import "/components/index.js"
 import { setCookie } from "/utils/cookie.js";
 
 
+// Temporary solution
+const USER_ID = "test";        // TODO : Update once a user service is implemented
+setCookie("userId", USER_ID);
+const USER_TOKEN = "74657374"; // TODO : Replace with the authentication token
+setCookie("userToken", USER_TOKEN);
+
+
+async function newPlayer() {
+    try {
+        const res = await fetch("/api/game-service/new-player", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({/* empty */}),
+        });
+        if (!res.ok) {
+            throw res.error;
+        }
+        const { playerId } = await res.json();
+        return playerId;
+    } catch (err) {
+        throw err;
+    }
+}
 
 async function startLocalMultiplayerGame() {
+    const playerId1 = await newPlayer();
+    const playerId2 = await newPlayer();
+
     try {
         const res = await fetch("/api/game-service/start-local-multiplayer-game", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                // TODO : Implement a way to generate distinct player ids (currently it's just raw value)
-                playerId1: 1,
-                playerId2: 2,
-            }),
+            body: JSON.stringify({playerId1, playerId2}),
         });
         if (!res.ok) {
             throw res.error;
