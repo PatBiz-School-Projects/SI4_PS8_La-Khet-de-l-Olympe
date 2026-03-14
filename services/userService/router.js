91cb2379eb72814443ab476edd0ce4124e517803
IsmailG <ismailguerraoui04@gmail.com>
2026-03-14 17:24:23 +0100

    fix(back): fixed entries in the users database

--- services/userService/router.js
+++ services/userService/router.js
@@ -1,18 +1,8 @@
-const { getDb } = require('./mongo');
+const handler = require('./handler');
 
 const routes = {
     "/api/users": async (request, response) => {
-        try {
-            const db = await getDb();
-            const users = await db.collection('users').find().toArray();
-
-            response.writeHead(200, { 'Content-Type': 'application/json' });
-            response.end(JSON.stringify(users));
-        } catch (error) {
-            console.error(error);
-            response.writeHead(500, { 'Content-Type': 'application/json' });
-            response.end(JSON.stringify({ error: 'Internal Server Error' }));
-        }
+        handler.createUser(request, response);
     }
 };
 
