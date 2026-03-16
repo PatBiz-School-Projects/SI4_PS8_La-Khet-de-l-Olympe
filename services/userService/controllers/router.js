12fb957dffb9ab4a44921df69a2026b553e04f50
IsmailG <ismailguerraoui04@gmail.com>
2026-03-16 22:10:28 +0100

    feat(back): added friendshipRoutes

--- services/userService/controllers/router.js
+++ services/userService/controllers/router.js
@@ -1,23 +1,42 @@
-const handler = require('./handler');
+const userHandler = require('./handler');
+const friendshipHandler = require('./friendshipHandler');
 
 const routes = {
     "/api/users": async (request, response) => {
-        await handler.createUser(request, response);
+        await userHandler.createUser(request, response);
     },
     "/api/users/connected/is-connected": async (request, response) => {
-        await handler.isUserConnected(request, response);
+        await userHandler.isUserConnected(request, response);
     },
     "/api/users/connected": async (request, response) => {
-        handler.getConnectedUsers(request, response);
+        userHandler.getConnectedUsers(request, response);
     },
     "/api/users/connect" : async (request, response) => {
-        await handler.connectUser(request, response);
+        await userHandler.connectUser(request, response);
     },
     "/api/users/disconnect": async (request, response) => {
-        await handler.disconnectUser(request, response);
+        await userHandler.disconnectUser(request, response);
     },
     "/api/users/profile" : async (request, response) => {
-        await handler.getProfile(request, response);
+        await userHandler.getProfile(request, response);
+    },
+    "/api/users/friends/request" : async (request, response) => {
+        await friendshipHandler.handleSendRequest(request, response);
+    },
+    "/api/users/friends/accept" : async (request, response) => {
+        await friendshipHandler.handleAcceptRequest(request, response);
+    },
+    "/api/users/friends/cancel": async (request, response) => {
+        await friendshipHandler.handleDeleteRequest(request, response);
+    },
+    "/api/users/friends/remove": async (request, response) => {
+        await friendshipHandler.handleDeleteRequest(request, response);
+    },
+    "/api/users/friends" : async (request, response) => {
+        await friendshipHandler.handleListFriends(request, response);
+    },
+    "/api/users/friends/requests" : async (request, response) => {
+        await friendshipHandler.handleListRequests(request, response);
     }
 };
 
