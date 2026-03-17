b14117ffe6b12ea807f14cd880510d258eedd943
IsmailG <ismailguerraoui04@gmail.com>
2026-03-17 20:30:26 +0100

    refactor(back): added methods before the routes

--- services/userService/controllers/router.js
+++ services/userService/controllers/router.js
@@ -2,49 +2,49 @@ const userHandler = require('./handler');
 const friendshipHandler = require('./friendshipHandler');
 
 const routes = {
-    "/api/users": async (request, response) => {
+    "POST /api/users": async (request, response) => {
         await userHandler.createUser(request, response);
     },
-    "/api/users/connected/is-connected": async (request, response) => {
+    "GET /api/users/connected/is-connected": async (request, response) => {
         await userHandler.isUserConnected(request, response);
     },
-    "/api/users/connected": async (request, response) => {
+    "GET /api/users/connected": async (request, response) => {
         userHandler.getConnectedUsers(request, response);
     },
-    "/api/users/connect" : async (request, response) => {
+    "POST /api/users/connect" : async (request, response) => {
         await userHandler.connectUser(request, response);
     },
-    "/api/users/disconnect": async (request, response) => {
+    "POST /api/users/disconnect": async (request, response) => {
         await userHandler.disconnectUser(request, response);
     },
-    "/api/users/profile" : async (request, response) => {
+    "GET /api/users/profile" : async (request, response) => {
         await userHandler.getProfile(request, response);
     },
-    "/api/users/friends/request" : async (request, response) => {
+    "POST /api/users/friends/request" : async (request, response) => {
         await friendshipHandler.handleSendRequest(request, response);
     },
-    "/api/users/friends/accept" : async (request, response) => {
+    "POST /api/users/friends/accept" : async (request, response) => {
         await friendshipHandler.handleAcceptRequest(request, response);
     },
-    "/api/users/friends/cancel": async (request, response) => {
+    "DELETE /api/users/friends/request": async (request, response) => {
         await friendshipHandler.handleDeleteRequest(request, response);
     },
-    "/api/users/friends/remove": async (request, response) => {
-        await friendshipHandler.handleDeleteRequest(request, response);
+    "DELETE /api/users/friends": async (request, response) => {
+        await friendshipHandler.handleDeleteFriend(request, response);
     },
-    "/api/users/friends" : async (request, response) => {
+    "GET /api/users/friends" : async (request, response) => {
         await friendshipHandler.handleListFriends(request, response);
     },
-    "/api/users/friends/requests" : async (request, response) => {
+    "GET /api/users/friends/requests" : async (request, response) => {
         await friendshipHandler.handleListRequests(request, response);
     }
 };
 
 async function manage(request, response) {
     const url = request.url;
-
-    if (routes[url]) {
-        await routes[url](request, response);
+    const routeKey = `${request.method} ${url}`
+    if (routes[routeKey]) {
+        await routes[routeKey](request, response);
     } else {
         response.writeHead(404, { 'Content-Type': 'application/json' });
         response.end(JSON.stringify({ error: 'Not Found' }));
