3a3f58bd5ff1606e8915747677565f9902333ccf
PatBiz <patrick.bizot@etu.univ-cotedazur.fr>
2026-03-29 20:03:11 +0200

    chore(back): enabled HTTPS server in prod

--- services/gateway/index.js
+++ services/gateway/index.js
@@ -1,8 +1,6 @@
 const http = require('http');
+const https = require('https');
 const httpProxy = require('http-proxy');
-
-// We need a proxy to send requests to the other services.
-const proxy = httpProxy.createProxyServer();
 
 const GAME_SERVICE_URL = process.env.GAME_SERVICE_URL;
 const FILE_SERVICE_URL = process.env.FILE_SERVICE_URL;
@@ -10,10 +8,15 @@ const AUTH_SERVICE_URL  = process.env.AUTH_SERVICE_URL;
 const USER_SERVICE_URL = process.env.USER_SERVICE_URL;
 const CHALLENGE_SERVICE_URL = process.env.CHALLENGE_SERVICE_URL;
 
-const server = http.createServer(function (request, response) {
+const IS_PROD = process.env.IS_PROD === "true";
+
+// Proxy to send requests to the other services.
+const proxy = httpProxy.createProxyServer();
+
+async function handleHTTPRequest(req, res) {
     // First, let's check the URL to see if it's a REST request or a file request.
     // We will remove all cases of "../" in the url for security purposes.
-    let filePath = request.url.split("/").filter(function(elem) {
+    let filePath = req.url.split("/").filter(function(elem) {
         return elem !== "..";
     });
 
@@ -22,41 +25,40 @@ const server = http.createServer(function (request, response) {
         if (filePath[1] === "api") {
             switch(filePath[2]) {
                 case "game-service":
-                    console.warn(`Deprecated request to game-service received: "${request.url}"`);
+                    console.warn(`Deprecated request to game-service received: "${req.url}"`);
                     /* continue in "games" case */
                 case "games":
                     console.log("-> Transfer request to game-service (8002)");
-                    proxy.web(request, response, { target: GAME_SERVICE_URL });
+                    proxy.web(req, res, { target: GAME_SERVICE_URL });
                     break;
                 case "auth":
                     console.log("-> Transfer request to auth-service (8003)");
-                    proxy.web(request, response, { target: AUTH_SERVICE_URL });
+                    proxy.web(req, res, { target: AUTH_SERVICE_URL });
                     break;
                 case "users":
                     console.log("-> Transfer request to user-service (8004)");
-                    proxy.web(request, response, { target : USER_SERVICE_URL });
+                    proxy.web(req, res, { target : USER_SERVICE_URL });
                     break;
                 case "challenge-service" :
                     console.log("-> Redirection challengeService (8005)");
-                    proxy.web(request, response, { target: CHALLENGE_SERVICE_URL });
+                    proxy.web(req, res, { target: CHALLENGE_SERVICE_URL });
                     break;
             }
 
         // If it doesn't start by /api, then it's a request for a file.
         } else {
             console.log("Request for a file received, transferring to the file service")
-            proxy.web(request, response, { target: FILE_SERVICE_URL });
+            proxy.web(req, res, { target: FILE_SERVICE_URL });
         }
     } catch(error) {
-        console.log(`Error while processing ${request.url}: ${error}`)
-        response.statusCode = 400;
-        response.end(`Something in your request (${request.url}) is strange...`);
+        console.log(`Error while processing ${req.url}: ${error}`)
+        res.statusCode = 400;
+        res.end(`Something in your request (${req.url}) is strange...`);
     }
-});
+}
 
-// Handle WebSocket upgrade requests
-server.on("upgrade", function (request, socket, head) {
-    let filePath = request.url.split("/").filter(function(elem) {
+function handleWebSocket(req, socket, head) {
+    let filePath = req.url.split("/").filter(function(elem) {
         return elem !== "..";
     });
 
@@ -67,15 +69,52 @@ server.on("upgrade", function (request, socket, head) {
                 /* continue in "games" case */
             case "games":
                 console.log("Transfer WS upgrade to game-service (8002)");
-                proxy.ws(request, socket, head, { target: GAME_SERVICE_URL });
+                proxy.ws(req, socket, head, { target: GAME_SERVICE_URL });
                 break;
             case "challenge-service":
                 console.log("-> WS Upgrade vers ChallengeService (8005)");
-                proxy.ws(request, socket, head, { target: CHALLENGE_SERVICE_URL });
+                proxy.ws(req, socket, head, { target: CHALLENGE_SERVICE_URL });
                 break;
         }
     }
-});
+}
 
-// For the server to be listening to request, it needs a port, which is set thanks to the listen function.
-server.listen(8000);
+if (IS_PROD) {
+    // HTTPS Certification
+    const HTTPS_CERTIFICATION = {
+        cert: process.env.SSL_CERTIFICATE.replace(/\\n/g, '\n'),
+        key:  process.env.SSL_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    };
+
+    // HTTP server to redirect client to the HTTPS server
+    const httpServer = http.createServer((req, res) => {
+        const httpsUrl = `https://${req.headers.host}${request.url}`;
+
+        const host = req.headers.host.replace(/:8000$/, ':8443');
+        const redirectUrl = `https://${host}${req.url}`;
+        console.log(`Redirect to HTTPS: ${req} --> ${redirectUrl}`);
+        res.writeHead(301, { 'Location': redirectUrl });
+        res.end();
+    });
+
+    // HTTPS server
+    const httpsServer = https.createServer(HTTPS_CERTIFICATION, handleHTTPRequest);
+
+    // Handle WebSocket upgrade requests
+    httpsServer.on("upgrade", handleWebSocket);
+
+    // For the servers to be listening to request, it needs a port, which is set thanks to the listen function.
+    httpServer.listen(8000);
+    httpsServer.listen(8443);
+} else {
+    // HTTP server
+    const httpServer = http.createServer(handleHTTPRequest);
+
+    // For the server to be listening to request, it needs a port, which is set thanks to the listen function.
+    httpServer.listen(8000);
+}
+
+
+
+
+
