1ba4fdf1814a8f9bd2904e9f08b2a703ddea3825
IsmailG <ismailguerraoui04@gmail.com>
2026-04-21 10:59:41 +0200

    refactor: cookies management implement to Android

--- frontend/src/utils/cookie.js
+++ frontend/src/utils/cookie.js
@@ -1,4 +1,11 @@
+import { Capacitor } from 'https://cdn.jsdelivr.net/npm/@capacitor/core@8.3.1/+esm';
+
+const isAndroid = Capacitor.getPlatform() === 'android';
+
 export function getCookie(name) {
+    if (isAndroid) {
+        return localStorage.getItem(name);
+    }
     const cookies = document.cookie.split(';');
     for (let cookie of cookies) {
         const [key, value] = cookie.trim().split('=');
@@ -7,26 +14,34 @@ export function getCookie(name) {
     return null;
 }
 
-
 export function setCookie(name, value) {
+    if (isAndroid) {
+        localStorage.setItem(name, value);
+        return;
+    }
     document.cookie = `${name}=${value}; path=/`;
 }
 
-
 export function removeCookie(name) {
+    if (isAndroid) {
+        localStorage.removeItem(name);
+        return;
+    }
     document.cookie = `${name}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
 }
 
-
 /** @alias {@link clearCookies()} */
 export function removeAllCookies() {
-    const cookies = document.cookie.split(";");
+    if (isAndroid) {
+        localStorage.clear();
+        return;
+    }
+    const cookies = document.cookie.split(';');
     for (let cookie of cookies) {
-        const name = cookie.split("=")[0].trim();
+        const name = cookie.split('=')[0].trim();
         removeCookie(name);
     }
 }
-
 
 /** @alias {@link removeAllCookies()} */
 export function clearCookies() {
